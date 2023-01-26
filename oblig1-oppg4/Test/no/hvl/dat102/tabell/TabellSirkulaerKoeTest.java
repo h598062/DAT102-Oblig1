8bd735ffeb8133dec3dfe0735789f42d0d50d4d6
Bjørnar <598062@stud.hvl.no>
2023-01-26 14:44:36 +0100

    gjort oppgave 4 c

--- oblig1-oppg4/Test/no/hvl/dat102/tabell/TabellSirkulaerKoeTest.java
+++ oblig1-oppg4/Test/no/hvl/dat102/tabell/TabellSirkulaerKoeTest.java
@@ -2,10 +2,31 @@ package no.hvl.dat102.tabell;
 
 import no.hvl.dat102.adt.KoeADT;
 import no.hvl.dat102.adt.KoeADTTest;
+import org.junit.jupiter.api.Test;
 
-public class TabellSirkulaerKoeTest extends KoeADTTest {
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class TabellSirkulaerKoeTest extends KoeADTTest {
 	@Override
 	protected KoeADT<Integer> reset() {
 		return new TabellSirkulaerKoe<>();
+	}
+
+	@Test
+	void utviderSeg() {
+		int SIZE = 10;
+		int utvidSIZE = (2*SIZE) + 7;
+		KoeADT<Integer> koe = new TabellSirkulaerKoe<>(SIZE);
+		for (int i = 0; i < utvidSIZE; i++) {
+			koe.innKoe(i);
+		}
+		int i = 0;
+		while (!koe.erTom()) {
+			koe.utKoe();
+			i++;
+		}
+		assertEquals(utvidSIZE, i);
+		assertTrue(koe.erTom());
 	}
 }
